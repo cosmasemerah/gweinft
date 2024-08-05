b886129c43054d329a0f86418b92a81091a45353
Waren Gonzaga <opensource@warengonzaga.com>
2024-08-05 12:08:19 +0800

    update settings for env

--- src/client.ts
+++ src/client.ts
@@ -4,10 +4,11 @@ import { defineChain } from "thirdweb";
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
+const chainId = import.meta.env.VITE_TEMPLATE_CHAIN_ID;
 const contractAddress = import.meta.env.VITE_TEMPLATE_CONTRACT_ADDRESS;
 const activeTokenId = import.meta.env.VITE_TEMPLATE_ACTIVE_TOKEN_ID;
 
-export const chain = defineChain(84532); // change this to your chain ID
+export const chain = defineChain(Number(chainId)); // change this to your chain ID
 export const contract = contractAddress;
 export const token = BigInt(activeTokenId);
 
